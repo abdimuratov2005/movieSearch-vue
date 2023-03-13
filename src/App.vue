91c2b6d521a750af38840998cd671bb794f9b5ce
Abdimuratov Abdulla <deshark550@gmail.com>
2023-03-13 11:51:25 +0500

    added IMDB search

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,85 @@
   <main class="app">
     <div class="app__container">
       <div class="app__form">
-        <input type="text" v-model="queryTitle" placeholder="Title...">
-        <button @click="fetchMovie">
-          <img :src="searchIcon">
-        </button>
+        <input type="text" @keypress.enter="fetchMovies" v-model="queryTitle" placeholder="Title...">
+        <input type="text" @keypress.enter="fetchIMDb" v-model="queryIMDb" placeholder="IMDb - For more info">
       </div>
       <TransitionGroup tag="div" name="fade">
-        <div class="app__result result" v-for="item in movie.Search" :key="item.id">
-          <div class="result__container">
-            <div class="result__img">
-              <img :src="item.Poster" alt="">
-            </div>
-            <div class="result__content">
-              <h1 class="result__title">
-                {{ item.Title }}
-              </h1>
-              <h4 class="result__year">
-                Released: {{ item.Year }}-year
-              </h4>
-              <h4 class="result__id">
-                ID: {{ item.imdbID }}
-              </h4>
-              <h4 class="result__type">
-                Type: {{ item.Type }}
-              </h4>
+        <div class="searchResult">
+          <div class="app__result result">
+            <div class="result__container" v-for="item in movies.Search" :key="item.id">
+              <div class="result__img">
+                <a :href="item.Poster" target="_blank">
+                  <img :src="item.Poster">
+                </a>
+              </div>
+              <div class="result__content">
+                <h1 class="result__title">
+                  {{ item.Title }}
+                </h1>
+                <h1 class="result__year">
+                  Released: {{ item.Year }}-year
+                </h1>
+                <h1 class="result__id">
+                  IMDb: {{ item.imdbID }}
+                </h1>
+                <h1 class="result__type">
+                  Type: {{ item.Type.toUpperCase() }}
+                </h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div v-if="movie.Error">
-          <h1 class="error">Not Found!</h1>
+          <div v-if="movies.Error">
+            <h1 class="error">Not Found!</h1>
+          </div>
+          <div class="app__result result" v-if="id">
+            <div class="result__container" v-for="item in result" :key="item.id">
+              <div class="result_id" v-if="item.Response == 'True'">
+                <div class="result__img">
+                  <a :href="item.Poster" target="_blank">
+                    <img :src="item.Poster">
+                  </a>
+                </div>
+                <div class="result__content">
+                  <h1 class="result__title">
+                    {{ item.Title }}
+                  </h1>
+                  <br>
+                  <h1 class="result__released">
+                    Released: {{ item.Released }}-year
+                  </h1>
+                  <br>
+                  <h1 class="result__genre">
+                    Genre: {{ item.Genre }}
+                  </h1>
+                  <br>
+                  <h1 class="result__rated">
+                    Rated: {{ item.Rated }}-year
+                  </h1>
+                  <br>
+                  <h1 class="result__type">
+                    Type: {{ item.Type.toUpperCase() }}
+                  </h1>
+                  <br>
+                  <h1 class="result__runtime">
+                    Runtime: {{ item.Runtime }}
+                  </h1>
+                  <br>
+                  <h1 class="result__language">
+                    Language: {{ item.Language }}
+                  </h1>
+                  <br>
+                  <h1 class="result__score" style="color: yellow;">
+                    Metascore: {{ item.Metascore }}
+                  </h1>
+                  <br>
+                </div>
+              </div>
+              <div v-else class="error">
+                <h1>Incorrect IMDb ID.</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </TransitionGroup>
     </div>
@@ -41,16 +91,28 @@
 import axios from 'axios'
 export default {
   data: () => ({
-    apiKey: 'ec04f4d0',
-    queryTitle: 'batman',
-    movie: {},
+    apiKey: 'YOUR_API_KEY',
+    queryTitle: '',
+    queryIMDb: '',
+    movies: {},
+    id: false,
     searchIcon: './public/search.svg',
+    result: {},
   }),
   methods: {
-    fetchMovie() {
+    fetchMovies() {
+      this.id = false
       axios(`https://www.omdbapi.com/?apikey=${this.apiKey}&s=${this.queryTitle}&plot=full`)
-        .then(response => this.movie = response.data)
+        .then(response => this.movies = response.data)
         .catch(err => alert(`Error, ${err}`))
+    },
+    fetchIMDb() {
+      this.movies = {}
+      this.id = true
+      axios(`https://www.omdbapi.com/?apikey=${this.apiKey}&i=${this.queryIMDb}&plot=full`)
+        .then(response => this.result[0] = response.data)
+        .catch(err => alert(`Error, ${err}`))
+
     },
   }
 }
@@ -75,20 +137,21 @@ export default {
   transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
 }
 
-/* 2. declare enter from and leave to state */
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
-  transform: scaleY(0.01) translate(30px, 0);
+  transform: scaleY(0.01);
 }
 
 .fade-leave-active {
   position: absolute;
 }
 
-.error{
+.error {
+  width: 100%;
   color: red;
   text-align: center;
+  text-transform: uppercase;
 }
 
 .app {
@@ -97,6 +160,7 @@ export default {
   justify-content: center;
   min-height: 100vh;
   padding: 30px 0px 0px 0px;
+  background: #00000060;
 
   &__container {
     width: 800px;
@@ -104,7 +168,7 @@ export default {
     padding: 30px 50px;
     border-radius: 30px 20px;
     background: rgb(2, 0, 36);
-    background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
+    background: linear-gradient(90deg, rgb(24, 22, 59) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%);
   }
 
   &__form {
@@ -137,9 +201,11 @@ export default {
   .result {
     margin: 30px 0;
 
-    &__container {
+    &__container,
+    &_id {
       display: flex;
       align-items: center;
+      width: 100%;
       justify-content: space-between;
     }
 
